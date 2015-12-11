9cedf7a527873d375499ba5bb14f12846bf76e37
“Ian <ianyhkwok@hotmail.com>
2015-12-11 19:07:23 +0800

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209093701) do
+ActiveRecord::Schema.define(version: 20151211092509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20151209093701) do
     t.boolean  "stock"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
